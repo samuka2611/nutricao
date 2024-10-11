806b2d3a514fc1eb1cd3be0eed2e8ee352f025d5
Samuel Moreira Nascimento <161725340+samuka2611@users.noreply.github.com>
2024-10-11 11:49:10 -0300

    11/10/2024

--- 2Tec-Nutricao-main/main.js
+++ 2Tec-Nutricao-main/main.js
@@ -37,3 +37,27 @@ if(pesoEhValido && alturaEhValida){
   tdImc.textContent = imc.toFixed(2);
  }
 }
+
+titulo.addEventListener("click", mostraMensagem);
+
+function mostraMensagem(){
+  console.log("O titulo foi clicado");
+}
+
+var botaoAdicionar = document.querySelector("Adicionar paciente");
+botaoAdicionar.addEventListener("click", function(event){
+  event.preventDefault();
+
+
+  var form = document.querySelector("#form-adiciona");
+
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value
+
+  console.log (form.nome.value);
+  console.log (form.altura.value);
+  console.log (form.peso.value);
+  console.log (form.gorduraCorporal.value);
+});
